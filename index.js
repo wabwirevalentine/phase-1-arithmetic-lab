c725bc6cab578f14ba083c6f6c9f3691cdfe0472
wabwirevalentine <valentine.wabwire@tracom.co.ke>
2025-03-18 09:46:56 +0300

    maths aritmetic

--- index.js
+++ index.js
@@ -1,2 +1,11 @@
-
 // Write your code here
+let num1 = 31,
+  num2 = 2;
+let multiply = num1 * num2;
+let random = Math.floor(Math.random() * 10) + 1;
+
+let num3 = 9;
+let num4 = 5;
+let mod = num3 % num4;
+
+let max = Math.max(...[10, 20, 5, 15]);
